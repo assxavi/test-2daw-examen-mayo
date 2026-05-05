1757696ba84b02daf5edd4dcd2930c2cc6266f8a
yeray <yerayval@gmail.com>
2026-05-05 12:37:37 +0200

    diseño de interfaces

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const appRoot = document.querySelector("#app");
 
-const subjectFiles = ["Acceso a Datos.json", "Desarrollo de Interfaces.json", "Digitalizacion.json", "IPE.json","DWES.json","sostenibilidad.json", "python.json","DWEC.json","despliegue.json", "Interfaces.json" ];
+const subjectFiles = ["Acceso a Datos.json", "Desarrollo de Interfaces.json", "Digitalizacion.json", "IPE.json","DWES.json","sostenibilidad.json", "python.json","DWEC.json","despliegue.json", "Interfaces.json","Diseño de Interfaces.json" ];
 const countOptions = [10, 20, 50, "all", "custom"];
 
 const initialState = {
